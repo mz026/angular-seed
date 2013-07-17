321aa5112fa5445272aac98a20d42d5de605e84f
Yang-Hsing Lin <yanghsing.lin@thekono.com>
2013-07-17 17:08:11 +0800

    add basic ui routing

--- app/js/app.js
+++ app/js/app.js
@@ -2,9 +2,18 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
-  config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-    $routeProvider.otherwise({redirectTo: '/view1'});
-  }]);
+angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers', "ui.state"]).config(
+  function($stateProvider) {
+
+  $stateProvider
+    .state("state1", {
+      url: "/view1"
+      , controller: "MyCtrl1"
+      , templateUrl: "partials/partial1.html"
+    })
+    .state("state2", {
+      url: "/view2"
+      , controller: "MyCtrl2"
+      , templateUrl: "partials/partial2.html"
+    });
+});
